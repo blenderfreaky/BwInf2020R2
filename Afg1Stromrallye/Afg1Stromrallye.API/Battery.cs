7caf40efffeae2edcb251a3175b352661e89f8da
blenderfreaky <blendcrafterxiv@gmail.com>
2019-12-25 00:07:32 +0100

    Edit

---
+++ Afg1Stromrallye/Afg1Stromrallye.API/Battery.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Afg1Stromrallye.API
+{
+    public class Battery
+    {
+        public Vector2Int Position { get; }
+        public Dictionary<Battery, IReadOnlyList<Vector2Int>> ShortestPaths { get; }
+        public Dictionary<Battery, (Vector2Int ExitSpot, Vector2Int IntermediateSpot)> Roundabouts { get; }
+
+        public Battery(Vector2Int position)
+        {
+            Position = position;
+            ShortestPaths = new Dictionary<Battery, IReadOnlyList<Vector2Int>>();
+            Roundabouts = new Dictionary<Battery, (Vector2Int ExitSpot, Vector2Int IntermediateSpot)>();
+        }
+
+        public void BuildShortestPaths(Board board)
+        {
+            //TODO: Use Dijkstra
+            foreach (var battery in board.Batteries)
+            {
+                var path = new List<Vector2Int>();
+
+                foreach (int x in Range(Position.X, battery.Position.X)) path.Add(new Vector2Int(x, Position.X));
+                foreach (int y in Range(Position.Y, battery.Position.Y)) path.Add(new Vector2Int(battery.Position.X, y));
+                path.Add(battery.Position);
+
+                ShortestPaths[battery] = path;
+                //TODO: Properly calculate this
+                Roundabouts[battery] = (path[1], path[2]);
+            }
+        }
+
+        private static IEnumerable<int> Range(int start, int end)
+        {
+            if (end < start)
+            {
+                for (int i = start; i > end; i--)
+                {
+                    yield return i;
+                }
+
+                yield break;
+            }
+
+            for (int i = start; i < end; i++)
+            {
+                yield return i;
+            }
+        }
+    }
+}
---
+++ Afg1Stromrallye/Afg1Stromrallye.API/Board.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+namespace Afg1Stromrallye.API
+{
+    public class Board
+    {
+        public int Width { get; }
+        public int Height { get; }
+        public IReadOnlyCollection<Battery> Batteries { get; }
+
+        public Board(int width, int height, IReadOnlyCollection<Vector2Int> batteryPositions)
+        {
+            Width = width;
+            Height = height;
+            Batteries = batteryPositions.Select(x => new Battery(x)).ToList();
+            foreach (var battery in Batteries) battery.BuildShortestPaths(this);
+        }
+
+        private static IEnumerable<Vector2Int> GetDirectNeighbours(Vector2Int position)
+        {
+            yield return position + new Vector2Int(0, -1);
+            yield return position + new Vector2Int(0, +1);
+            yield return position + new Vector2Int(-1, 0);
+            yield return position + new Vector2Int(+1, 0);
+        }
+
+        public IEnumerable<Vector2Int> GetVisitable(Vector2Int position) =>
+            GetDirectNeighbours(position)
+            .Where(x => Contains(x) && !Batteries.Any(y => y.Position == x));
+
+        public bool Contains(Vector2Int position) =>
+            position.X >= 0 && position.X < Width
+            && position.Y >= 0 && position.Y < Height;
+    }
+}
